91343bf66c1551003d9ff7872e5e9270034fd54e
Seth Brock <sethabrock@gmail.com>
2024-11-06 06:21:00 +0000

    Updated HTML

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let express = require("express");
-let app = new express();                                                                     
+let app = new express();
 
 // set up database connection
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"concert-db.c1o0umomwvoq.us-east-1.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "adminadmin",
   database:"paradise-concerts",
   port: 3306,
  },
@@ -18,8 +18,12 @@ app.get("/",(req,res) => {
  .select()
  .from("venues")
  .then((result) => {
-  console.log(result);
+  let html = "<body><ul>";
+  for (let i=0; i < result.length; i++) {
+    html += "<li>" + result[i].location + "</li>";
+  }
+  html += "</ul></body>"
   res.send(result);
- }); 
+ });
 });
 app.listen(3000);
